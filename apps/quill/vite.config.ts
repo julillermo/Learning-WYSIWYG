5c6c813e0aa32f0f645726d73875fa4b6fccaa2e
TulioG_Projects <tuliog.projects@gmail.com>
2026-02-16 02:27:13 +0800

    start base config

---
+++ apps/quill/vite.config.ts
@@ -0,0 +1,15 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    vanillaExtractPlugin(),
+  ],
+});
